6e8d733b4c4fae7296a7f7a284ea44d6206278e9
Loránd Biró <lorandbiro@outlook.com>
2014-12-02 19:42:53 +0100

    NuGet.exe deleted, build script downloads it. Script also changed to use the framework's MSBuild.

--- Nuget/BuildPackages.ps1
+++ Nuget/BuildPackages.ps1
@@ -1,6 +1,17 @@
-&.\NuGet.exe restore ..\Source\LogSpect.sln
-&"c:\Program Files (x86)\MSBuild\12.0\Bin\amd64\MSBuild.exe" ..\Source\LogSpect.sln /t:Rebuild /p:Configuration=Release
+# Disable the progress bar because it slows down Invoke-WebRequest
+$progressPreference = 'silentlyContinue'
 
+# Download NuGet.exe
+Invoke-WebRequest https://nuget.org/nuget.exe -OutFile NuGet.exe
+
+# Restore NuGet packages
+&.\NuGet.exe restore ..\Source\LogSpect.sln
+
+# Build solution
+$msbuildPath = Join-Path $env:windir Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+&$msbuildPath ..\Source\LogSpect.sln /t:Rebuild /p:Configuration=Release
+
+# Prepare output directory
 if (Test-Path .\Packages)
 {
     Remove-Item .\Packages\*
@@ -10,6 +21,7 @@ else
     New-Item .\Packages -ItemType Directory
 }
 
+# Create packages
 &.\NuGet.exe pack .\LogSpect\LogSpect.nuspec -OutputDirectory Packages
 &.\NuGet.exe pack .\LogSpect.Core\LogSpect.Core.nuspec -OutputDirectory Packages
 &.\NuGet.exe pack .\LogSpect.BasicLoggers\LogSpect.BasicLoggers.nuspec -OutputDirectory Packages
