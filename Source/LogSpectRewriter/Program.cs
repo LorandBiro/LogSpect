329646be5ed23c58bb8f558dce09cb9ceddf10e6
Loránd Biró <lorandbiro@outlook.com>
2014-12-03 01:29:01 +0100

    New exit code for missing input file added to LogSpectRewriter.

--- Source/LogSpectRewriter/Program.cs
+++ Source/LogSpectRewriter/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace LogSpectRewriter
 {
     using System;
+    using System.IO;
     using LogSpectRewriter.Output;
     using LogSpectRewriter.Rewriting;
 
@@ -17,16 +18,23 @@
                 Console.WriteLine("Possible exit codes:");
                 Console.WriteLine("    0 - Rewrite was successful");
                 Console.WriteLine("    1 - Invalid arguments");
-                Console.WriteLine("    2 - Unexpected exception");
+                Console.WriteLine("    2 - Input file doesn't exist");
+                Console.WriteLine("    3 - Unexpected exception");
                 return 1;
             }
 
             string inputAssemblyPath = args[0];
             string outputAssemblyPath = args.Length > 1 ? args[1] : inputAssemblyPath;
 
+            if (!File.Exists(inputAssemblyPath))
+            {
+                Console.WriteLine("Input file doesn't exist: {0}", inputAssemblyPath);
+                return 2;
+            }
+
             AssemblyRewriter rewriter = new AssemblyRewriter(new ConsoleOutputWriter());
             bool success = rewriter.TryRewriteAssembly(inputAssemblyPath, outputAssemblyPath);
-            return success ? 0 : 2;
+            return success ? 0 : 3;
         }
     }
 }
