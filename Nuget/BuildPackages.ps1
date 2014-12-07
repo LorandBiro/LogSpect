23547179b520d785fa0320e92b600a86f9022c10
Loránd Biró <lorandbiro@outlook.com>
2014-12-07 15:29:05 +0100

    Folders for nuspec files removed.

--- Nuget/BuildPackages.ps1
+++ Nuget/BuildPackages.ps1
@@ -22,7 +22,7 @@ else
 }
 
 # Create packages
-&.\NuGet.exe pack .\LogSpect\LogSpect.nuspec -OutputDirectory Packages
-&.\NuGet.exe pack .\LogSpect.Core\LogSpect.Core.nuspec -OutputDirectory Packages
-&.\NuGet.exe pack .\LogSpect.BasicLoggers\LogSpect.BasicLoggers.nuspec -OutputDirectory Packages
-&.\NuGet.exe pack .\LogSpect.NLog\LogSpect.NLog.nuspec -OutputDirectory Packages
+&.\NuGet.exe pack .\LogSpect.nuspec -OutputDirectory Packages
+&.\NuGet.exe pack .\LogSpect.Core.nuspec -OutputDirectory Packages
+&.\NuGet.exe pack .\LogSpect.BasicLoggers.nuspec -OutputDirectory Packages
+&.\NuGet.exe pack .\LogSpect.NLog.nuspec -OutputDirectory Packages
