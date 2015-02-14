e44d9438788835194d4ebdcab7767134b2c59bb9
Loránd Biró <lorandbiro@outlook.com>
2015-02-14 11:19:25 +0100

    Copyright information is updated.

--- Source/LogSpect.Core/Properties/AssemblyInfo.cs
+++ Source/LogSpect.Core/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LogSpect.Core")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyCopyright("Copyright © Loránd Biró 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
--- Source/LogSpect.Log4Net/LogSpect.Log4Net (Net40-Client)/Properties/AssemblyInfo.cs
+++ Source/LogSpect.Log4Net/LogSpect.Log4Net (Net40-Client)/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LogSpect.Log4Net")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Loránd Biró 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
